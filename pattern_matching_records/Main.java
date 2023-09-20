d066adfc2ebe3003ee86d1ff997f5494c189c39f
James Young <james.w.young@bjss.com>
2023-09-20 14:09:03 +0100

    Pattern matching with records example

---
+++ pattern_matching_records/Main.java
@@ -0,0 +1,31 @@
+package pattern_matching_records;
+
+import pattern_matching_records.Point;
+public class Main {
+
+    public static void main(String ... args) {
+        System.out.println("Testing Pattern Matching with Records!");
+
+
+        Point p = new Point(1, 2, 3);
+
+        if (p instanceof Point q) {
+            System.out.println(q.x());
+            System.out.println(q.y());
+            System.out.println(q.z());
+        }
+
+
+//        My concern with this is that it couples the pattern matching to how the parameters are ordered in the original
+//        record class, potentially introducing brittleness (albeit no more so than the record constructor)
+        if (p instanceof Point(int x, int y, int r)) {
+            System.out.println(x);
+            System.out.println(y);
+            System.out.println(r);
+        }
+
+
+
+    }
+
+}
---
+++ pattern_matching_records/Point.java
@@ -0,0 +1,3 @@
+package pattern_matching_records;
+public record Point(int x, int y, int z) {
+}
