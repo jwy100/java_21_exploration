8043e18a96eb2dc52e0e119adfae0ac250ae420c
James Young <james.w.young@bjss.com>
2023-09-21 10:45:19 +0100

    Testing the exhaustive switch restrictions

--- pattern_matching_for_switch/Main.java
+++ pattern_matching_for_switch/Main.java
@@ -1,19 +1,50 @@
 public class Main {
 
+
+    public sealed interface Card permits Suit {
+
+    }
+
+    public enum Suit implements Card {
+        HEARTS, DIAMONDS, SPADES, CLUBS
+    }
+
     public static void main(String[] args) {
 
-        stringTest(args[0]);
+        declareSuit(Suit.DIAMONDS);
 
     }
 
 
 
+    public static void declareSuitExhaustive(Card o) {
+        switch (o) {
+            case Suit.HEARTS -> System.out.println("It's Hearts!");
+            case Suit.SPADES -> System.out.println("It's Spades!");
+            case Suit.CLUBS -> System.out.println("It's Clubs!");
+            case Suit.DIAMONDS -> System.out.println("It's Diamonds!");
+        }
+    }
 
-    public static void declare(Object o) {
+    // Hmm, exhaustive switch is not enforced for all Suits
+    public static void declareSuit(Suit o) {
+        switch (o) {
+            case Suit.HEARTS -> System.out.println("It's Hearts!");
+            case Suit.SPADES -> System.out.println("It's Spades!");
+            case Suit.CLUBS -> System.out.println("It's Clubs!");
+        }
+    }
+
+    public static void colonStyleSwitch(Object o) {
         switch (o) {
             case null ->  System.out.println("It's null!");
             case Integer i -> System.out.println("It's an integer!");
             case String s -> System.out.println("It's a string!");
+            case Suit.HEARTS -> System.out.println("It's Hearts!");
+            case Suit.SPADES -> System.out.println("It's Spades!");
+            case Suit.CLUBS -> System.out.println("It's Clubs!");
+            case Suit.DIAMONDS -> System.out.println("It's Diamonds!");
+
             default -> System.out.println("Hmm ... Not sure what that is ...");
         }
     }
@@ -22,11 +53,15 @@ public class Main {
     public static void stringTest(String text) {
         switch (text) {
             case null -> {}
-            case String s
-            when s.equalsIgnoreCase("YES") -> System.out.println("Awesome!");
-            case String s
-            when s.equalsIgnoreCase("NO") -> System.out.println("Shame :(");
-            case String s -> System.out.println("Sorry, didn't catch that");
+            case String s when s.equalsIgnoreCase("YES") -> {
+                System.out.println("Awesome!");
+            }
+            case String s when s.equalsIgnoreCase("NO") -> {
+                System.out.println("Shame :(");
+            }
+            case String s -> {
+                System.out.println("Sorry, didn't catch that");
+            }
         }
     }
 }
